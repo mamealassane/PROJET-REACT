52ca1a8e8605adf7b37955438ca16218c34df70e
mamealassane <lahialassane99@gmail.com>
2023-09-25 01:04:10 +0000

    REACT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Nav, Container} from 'react-bootstrap'
+import './style.css';
+import Card from 'react-bootstrap/Card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const App = () => {
+    return (
+        <div className='App'>
+        
+            {/* creation navbar */}
+    <>
+    <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+        <Navbar.Brand href="#home">Projet-React</Navbar.Brand>
+        <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">About</Nav.Link>
+            <Nav.Link href="#pricing">Contact</Nav.Link>
+        </Nav>
+        </Container>
+    </Navbar>
+    
+    </>
+    {/* the heading */}
+
+    <h1> React-projet</h1>
+    
+
+    {/* parti card */}
+    <div className='card'>
+    
+    <>
+    {[
+        'Primary',
+        'Secondary',
+        'Success',
+        
+    ].map((variant) => (
+        <Card
+        bg={variant.toLowerCase()}
+        key={variant}
+        text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+        style={{ width: '18rem' } }
+        className="mb-2"
         >
-          Learn React
-        </a>
-      </header>
+        <Card.Header>Header</Card.Header>
+        <Card.Body>
+            <Card.Title>{variant} Card Title </Card.Title>
+            <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+            </Card.Text>
+        </Card.Body>
+        </Card>
+    ))}
+    </>
+
     </div>
-  );
-}
+    
+        </div>
+    );
+};
 
 export default App;
+
